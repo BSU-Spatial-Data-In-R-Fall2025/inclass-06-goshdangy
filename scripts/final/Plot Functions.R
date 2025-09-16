e4c1dd778c7d75d20c374c8edda3ff02a296a770
Caleb Ashcraft <CALEBASHCRAFT@boisestate.edu>
2025-09-15 22:16:53 -0600

    Added Color customization and scaled graph

--- scripts/final/Plot Functions.R
+++ scripts/final/Plot Functions.R
@@ -1,25 +1,30 @@
 ##ggplot for top 5 Donors
 
 #Function for Top 5 Donors
-plot_top_donors <- function(df) {
-  donor_split <- split(df, df$representative)
-  
-  map(donor_split, function(rep_df) {
-    ggplot(rep_df, aes(x = reorder(contributor_name, total), y = total)) +
-      geom_col(fill = "red") +
-      coord_flip() +
-      labs(
-        title = paste("Top 5 Donors -", str_to_title(rep_df$representative[1])),
-        x = "Donor Name",
-        y = "Amount Given ($)"
-      ) +
-      theme_linedraw()
-  })
+
+plot_top_donors <- function(df, fill_color = "red", plot_theme = theme_linedraw()) {
+  df %>%
+    split(.$representative) %>%  # Splitting the data into a list of tibbles, one per representative
+   
+    # For each representative's data frame, create a plot 
+    map(~ {
+      max_val <- max(df$total, na.rm = TRUE) + 1000
+      ggplot(.x, aes(
+        x = reorder(contributor_name, total), y = total)) +  # Reorder bars by total amount
+        geom_col(fill = fill_color) +  #User can define Color or default red
+        scale_y_continuous(limits = c(0, max_val))+
+        labs(
+          title = paste("Top 5 Donors -", str_to_title(unique(.x$representative))),
+          x = "Donor Name",
+          y = "Amount Given ($)"
+        ) +
+        plot_theme
+    })
 }
 
-#plug our data
-rep_plots <- plot_top_donors(top5_donors)
 
-# View Fulcer's plot
-# Can use following Names to Check ("Simpson", "Fulcher", "Risch", "Crapo")
-rep_plots[["fulcher"]]
+#plug our data
+rep_plots <- plot_top_donors(top5_donors, "violet")
+
+# View Fulcer's plot; Can use following Names to Check ("simpson", "fulcher", "risch", "crapo")
+rep_plots[["risch"]]
